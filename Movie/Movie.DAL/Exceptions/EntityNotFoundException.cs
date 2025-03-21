8f323ecb4bc046f78860148d9e93331e7c5b6650
VladPaliychuk <ssmxtxl@gmail.com>
2025-03-21 17:24:28 +0200

    add EntityNotFoundException

--- Movie/Movie.DAL/Exceptions/EntityNotFoundException.cs
+++ Movie/Movie.DAL/Exceptions/EntityNotFoundException.cs
@@ -1,6 +1,14 @@
 namespace Movie.DAL.Exceptions;
 
-public class EntityNotFoundException
+public class EntityNotFoundException : Exception
 {
-    
+    public EntityNotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public EntityNotFoundException()
+        : base()
+    {
+    }
 }
